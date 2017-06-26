a36b15a9dd209976b78a59674953f8af738a7351
Lucas Brandt <Lucas.Brandt@Domain.GlobalADS.priv>
2017-06-26 15:03:56 -0400

    Initial commit

---
+++ aqbot.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from tkinter import *
+
+currentVersion = "0.1"
+browser = webdriver.Chrome(executable_path="chromedriver.exe")
+
+app = Tk()
+app.title("aqBot v" + currentVersion)
+app.minsize(width=300, height=200)
+openButton = Button(app, text = "Open AdventureQuest")
+openButton.pack()
+openButtonGuardian = Button(app, text = "Open AdventureQuest (Guardians)")
+openButtonGuardian.pack()
+killButton = Button(app, text = "Kill Opponent")
+killButton.pack()
+
+def openBrowser():
+    browser.get('https://aq.battleon.com/Build30/game.asp?launchtype=medium/')
+
+def openBrowserGuardian():
+    browser.get('https://guardian.battleon.com/Build30/game.asp?launchtype=medium/')
+
+def killOpponent():
+    browser.execute_script("javascript:document.embeds[0].SetVariable(\"_root.monster.intHP\", 0")
+
+def exitHandler():
+    browser.quit()
+    app.destroy()
+
+openButton.configure(command=openBrowser)
+openButtonGuardian.configure(command=openBrowserGuardian)
+killButton.configure(command=killOpponent)
+
+app.protocol("WM_DELETE_WINDOW", exitHandler)
+app.mainloop()
