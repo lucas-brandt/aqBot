96ab7947f2df813994a2be65e07b796d7afc197c
Lucas Brandt <Lucas.Brandt@Domain.GlobalADS.priv>
2017-06-27 10:41:45 -0400

    version 0.2 release, added infinite stats button, fixed bug with the specific stat buttons not working, and added a repeatable timer class

--- aqbot.py
+++ aqbot.py
@@ -1,25 +1,54 @@
 from selenium import webdriver
 from tkinter import *
+from threading import Timer
 
-currentVersion = "0.1.1"
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+CURRENT_VERSION = "0.2"
+
+buttonSwitch = True
 browser = webdriver.Chrome(executable_path="chromedriver.exe")
 
 app = Tk()
-app.title("aqBot v" + currentVersion)
+app.title("aqBot v" + CURRENT_VERSION)
 app.minsize(width=300, height=200)
 openButton = Button(app, text = "Open AdventureQuest")
 openButton.pack()
 openButtonGuardian = Button(app, text = "Open AdventureQuest (Guardians)")
 openButtonGuardian.pack()
 
-killButton = Button(app, text = "Kill Opponent")
+killButton = Button(app, text="Kill Opponent")
 killButton.pack()
-mpButton = Button(app, text = "Refill MP for one turn")
+mpButton = Button(app, text="Refill MP for one turn")
 mpButton.pack()
-spButton = Button(app, text = "Refill SP for one turn")
+spButton = Button(app, text="Refill SP for one turn")
 spButton.pack()
-healButton = Button(app, text = "Full Heal")
+healButton = Button(app, text="Full Heal")
 healButton.pack()
+infiniteButton = Button(app, text="Infinite HP/SP/MP")
+infiniteButton.pack()
 
 
 def openBrowser():
@@ -40,6 +69,27 @@ def refillSP():
 def fullHeal():
     browser.execute_script("document.embeds[0].SetVariable(\"_root.player.intHP\", 9999)")
 
+def infiniteStats():
+    refillMP()
+    refillSP()
+    fullHeal()
+
+infiniteStatsTimer = RepeatedTimer(1, infiniteStats)
+
+def infiniteStatsControl():
+    global infiniteStatsTimer
+    global infiniteButton
+    global buttonSwitch
+
+    if buttonSwitch:
+        buttonSwitch = False
+        infiniteButton.configure(text="Stop")
+        infiniteStatsTimer.start()
+    else:
+        buttonSwitch = True
+        infiniteButton.configure(text="Infinite HP/SP/MP")
+        infiniteStatsTimer.stop()
+        
 def exitHandler():
     browser.quit()
     app.destroy()
@@ -47,6 +97,10 @@ def exitHandler():
 openButton.configure(command=openBrowser)
 openButtonGuardian.configure(command=openBrowserGuardian)
 killButton.configure(command=killOpponent)
+mpButton.configure(command=refillMP)
+spButton.configure(command=refillSP)
+healButton.configure(command=fullHeal)
+infiniteButton.configure(command=infiniteStatsControl)
 
 app.protocol("WM_DELETE_WINDOW", exitHandler)
 app.mainloop()
