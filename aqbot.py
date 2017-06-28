81eb341fe5bd9eff483ed4d51d49db00509fa39d
Lucas <lucasrbrandt@gmail.com>
2017-06-27 22:08:40 -0400

    added functionality for automatic mouse movements and clicks

--- aqbot.py
+++ aqbot.py
@@ -1,44 +1,23 @@
 from selenium import webdriver
 from tkinter import *
 from threading import Timer
+from repeatedTimer import RepeatedTimer
 
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer = None
-        self.interval = interval
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.is_running = False
-        
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-CURRENT_VERSION = "0.2"
+CURRENT_VERSION = "0.2.1"
 
 buttonSwitch = True
 browser = webdriver.Chrome(executable_path="chromedriver.exe")
 
 app = Tk()
 app.title("aqBot v" + CURRENT_VERSION)
-app.minsize(width=300, height=200)
+app.minsize(width=300, height=300)
 openButton = Button(app, text = "Open AdventureQuest")
 openButton.pack()
 openButtonGuardian = Button(app, text = "Open AdventureQuest (Guardians)")
 openButtonGuardian.pack()
 
+zeroButton = Button(app, text="Set Opponent Health to 0")
+zeroButton.pack()
 killButton = Button(app, text="Kill Opponent")
 killButton.pack()
 mpButton = Button(app, text="Refill MP for one turn")
@@ -57,7 +36,7 @@ def openBrowser():
 def openBrowserGuardian():
     browser.get('https://guardian.battleon.com/Build30/game.asp?launchtype=medium/')
 
-def killOpponent():
+def setToZero():
     browser.execute_script("document.embeds[0].SetVariable(\"_root.monster.intHP\", 0)")
 
 def refillMP():
@@ -89,6 +68,15 @@ def infiniteStatsControl():
         buttonSwitch = True
         infiniteButton.configure(text="Infinite HP/SP/MP")
         infiniteStatsTimer.stop()
+
+def killMonster():
+    setToZero()
+    game = browser.find_element_by_tag_name("td")
+
+    actions = webdriver.ActionChains(browser)
+    actions.move_to_element_with_offset(game, 400, 200)
+    actions.click()
+    actions.perform()
         
 def exitHandler():
     browser.quit()
@@ -96,7 +84,8 @@ def exitHandler():
 
 openButton.configure(command=openBrowser)
 openButtonGuardian.configure(command=openBrowserGuardian)
-killButton.configure(command=killOpponent)
+zeroButton.configure(command=setToZero)
+killButton.configure(command=killMonster)
 mpButton.configure(command=refillMP)
 spButton.configure(command=refillSP)
 healButton.configure(command=fullHeal)
